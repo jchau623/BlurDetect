3c08a7fc0b38a662f8411a0b8570ef138a3ee483
Justin Chau <justin.chau623@gmail.com>
2017-11-24 23:17:50 -0800

    Downsize images to 1000 on longest side for faster processing

--- src/read_raws.py
+++ src/read_raws.py
@@ -3,12 +3,29 @@ import rawpy
 import time
 import cv2
 
+### Function definitions
+def resizeToMax1000Shape(image):
+	shape = image.shape;
+	if shape[0] > shape[1]:
+		high = shape[0]
+	else:
+		high = shape[1]
+	if high < 1000:
+		return image
+	else:
+		factor = 1000/high
+		downsized = cv2.resize(image, (0,0), fx=factor, fy=factor)
+		return downsized
+###
+
 start = time.time()
 
 if len(sys.argv) > 2:
 	print("One image at a time, please.")
+	exit()
 elif len(sys.argv) == 1:
 	print("Please provide an image.")
+	exit()
 
 path = sys.argv[1]
 try:
@@ -24,7 +41,12 @@ if img is None:
 	exit()
 else:
 	print(path)
+
 #grayscale = (0.21*img[:,:,0])+(0.72*img[:,:,0])+(0.07*img[:,:,0])
+print("Shape before downsize: " + str(img.shape))
+# downsize the image for speed performance
+img = resizeToMax1000Shape(img)
+print("Shape after downsize: " + str(img.shape))
 grayscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 # Histogram equalization: https://docs.opencv.org/2.4/doc/tutorials/imgproc/histograms/histogram_equalization/histogram_equalization.html
 # Or if the image is dark, don't equalize histogram? So no seriousbanding or noise is introduced.
