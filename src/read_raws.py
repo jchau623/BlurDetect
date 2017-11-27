5e8171085009933fee549fca985a809d43e4e0a1
Justin Chau <justin.chau623@gmail.com>
2017-11-26 22:12:26 -0800

    Added in 3 more focus measure scoring functions

--- src/read_raws.py
+++ src/read_raws.py
@@ -41,6 +41,29 @@ def adjust_gamma(image, gamma):
  
 	# apply gamma correction using the lookup table
 	return cv2.LUT(image, table)
+
+def laplacianVariance(image):
+	return cv2.Laplacian(image, cv2.CV_64F).var()
+
+def modifiedLaplacian(image):
+	M = np.array([[-1.], [2.], [-1.]])
+	G = cv2.getGaussianKernel(3, -1, cv2.CV_64F)
+	Lx = cv2.sepFilter2D(image, -1, M, G)
+	Ly = cv2.sepFilter2D(image, -1, G, M)
+	FM = np.abs(Lx) + np.abs(Ly)
+	return cv2.mean(FM)[0]
+
+def tenengrad(image, ksize):
+	Gx = cv2.Sobel(image, cv2.CV_64F, 1, 0, ksize)
+	Gy = cv2.Sobel(image, cv2.CV_64F, 0, 1, ksize)
+	FM = np.square(Gx) + np.square(Gy)
+	return cv2.mean(FM)[0]
+
+def GLVN(image):
+	m = cv2.meanStdDev(image)
+	mu = m[0][0]
+	sigma = m[1][0]
+	return (sigma[0] * sigma[0]) / mu[0]
 ###
 
 start = time.time()
@@ -75,19 +98,24 @@ grayscale = convertToGrayscale(img)
 grayscale = adjust_gamma(grayscale, 3)
 equalized_grayscale = applyCLAHE(3.0, (10,10), grayscale)
 # Histogram equalization means noise is introduced.
-denoised = applyDenoising(equalized_grayscale, 10, 21, 7)
+denoised = applyDenoising(equalized_grayscale, 10, 7, 21)
 cv2.imwrite("test.jpg", grayscale)
 cv2.imwrite("test_eq.jpg", equalized_grayscale)
 cv2.imwrite("test_eq_denoised.jpg", denoised)
 # Variance of Laplacian is a measure of focus
-var = cv2.Laplacian(img, cv2.CV_64F).var()
-var_lap = cv2.Laplacian(grayscale, cv2.CV_64F).var()
-var_lap_eq = cv2.Laplacian(equalized_grayscale, cv2.CV_64F).var()
-var_lap_eq_denoised = cv2.Laplacian(denoised, cv2.CV_64F).var()
+var = laplacianVariance(img)
+var_lap = laplacianVariance(grayscale)
+var_lap_eq = laplacianVariance(equalized_grayscale)
+var_lap_eq_denoised = laplacianVariance(denoised)
 print("Score: " + str(var))
 print("Score with grayscale: " + str(var_lap))
 print("Score with equalized histogram: " + str(var_lap_eq))
 print("Score with denoising: " + str(var_lap_eq_denoised))
-print("Score: " + str(var))
+modified_lap = modifiedLaplacian(denoised)
+print("modifiedLaplacian: " + str(modified_lap))
+tenengrad_score = tenengrad(denoised, 7)
+print("Tenengrad: " + str(tenengrad_score))
+glvn_score = GLVN(denoised)
+print("GLVN: " + str(glvn_score))
 end = time.time()
 print("Time elapsed: " + str(end - start) + "\n")
