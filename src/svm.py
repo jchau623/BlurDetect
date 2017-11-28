c5706cd21dd2f6e1086ef8a6e3004f1567b4ce21
Justin Chau <justin.chau623@gmail.com>
2017-11-27 17:54:52 -0800

    Uploading training data

--- src/svm.py
+++ src/svm.py
@@ -4,6 +4,4 @@ import numpy as np
 lin_clf = svm.LinearSVC()
 DS_X = np.load("../training_np_arrays/X.npy")
 DS_Y = np.load("../training_np_arrays/Y.npy")
-print(DS_X)
-print(DS_Y)
 lin_clf.fit(DS_X, DS_Y)
